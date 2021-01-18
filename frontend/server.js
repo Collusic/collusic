b68dd32529e908059316473283d7d56092add425
Daae-Kim <wlsemrdl1212@naver.com>
2021-01-18 22:38:09 +0900

    page5 and create udate

--- frontend/server.js
+++ frontend/server.js
@@ -3,9 +3,21 @@ const app = express();
 var fs = require("fs");
 const port = 3000;
 var bodyParser = require("body-parser");
+
+var mysql = require("mysql");
+var db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "123456",
+  database: "pagenew",
+});
+db.connect();
+app.use(express.static("Active"));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("Follow"));
 app.use(express.static("image"));
 app.use(express.static("Unfollow"));
+
 app.get("/", (req, res) => {
   var page1 = `
   <!DOCTYPE html>
@@ -325,10 +337,15 @@ app.get("/page3", (req, res) => {
 });
 
 app.get("/page4", (req, res) => {
-  var html = `
-        
+  var title = req.query.id;
+  var page4 = `
+  <a href="/create">create</a> <a href="/update?id=${req.query.id}">update</a>
+  <form action="/delete_process" method="post">
+    <input type="hidden" name="id" value="${title}">
+    <input type="submit" value="delete">
+  </form>
     `;
-  res.send(html);
+  res.send(page4);
 });
 
 app.get("/page5", function (req, res) {
@@ -413,6 +430,80 @@ app.get("/page5", function (req, res) {
     </html>
     `;
   res.send(page5);
+});
+
+app.get("/create", function (req, res) {
+  db.query(`SELECT * FROM project;`, function (error, projects) {
+    if (error) {
+      throw error;
+    }
+    var template = `
+  <form action="/create_process" method="post">
+    <p><input type="text" name="title" placeholder="title"></p>
+    <p>
+      <textarea name="description" placeholder="description"></textarea>
+      </p>
+      <p>
+        <input type="submit">
+      </p>
+    </form>
+    `;
+    res.writeHead(200);
+    res.send(template);
+  });
+});
+app.post("/create_process", function (req, res) {
+  //   var title = req.body.title;
+  //   var description = req.body.description;
+  db.query(
+    `
+            INSERT INTO project (key, u_key, path, title, description) 
+              VALUES(1, 2, dfdfdf, ?, ?)`,
+    [post.title, post.description],
+    function (error, result) {
+      res.writeHead(302, { Location: `/?id=${result.insertId}` });
+      res.end();
+    }
+  );
+});
+
+app.get("/update", function (req, res) {
+  fs.readFile(`Active/${req.query.id}`, "utf-8", function (err, description) {
+    var jaemook = req.query.id;
+    var template = `
+      <form action="/update_process" method="post">
+        <input type="hidden" name="id" value="${jaemook}">
+        <p><input type="text" name="title" placeholder="title" value="${jaemook}"></p>
+        <p>
+          <textarea name="description" placeholder="description">${description}</textarea>
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+        </form>
+        <a href="/create">create</a> <a href="/update?id=${jaemook}">update</a>`;
+    res.send(template);
+  });
+});
+app.post("/update_process", function (req, res) {
+  var id = req.body.id;
+  var title = req.body.title;
+  var description = req.body.description;
+
+  fs.rename(`Active/${id}`, `Active/${title}`, function (err) {
+    fs.writeFile(`Active/${title}`, description, "utf-8", function (err) {
+      res.writeHead(302, { Location: `/?id=${title}` });
+      res.end();
+    });
+  });
+});
+
+app.post("/delete_process", function (req, res) {
+  var id = req.body.id;
+  fs.unlink(`Active/${id}`, function (err) {
+    res.writeHead(302, { Location: `/` });
+    res.end();
+  });
 });
 
 app.listen(3000, function () {
