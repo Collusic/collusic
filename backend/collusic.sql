5420a81e2b79de2b4872209057ba6d555549577b
Daae-Kim <wlsemrdl1212@naver.com>
2021-01-26 23:02:46 +0900

    push collusic.sql

--- backend/collusic.sql
+++ backend/collusic.sql
@@ -78,3 +78,22 @@ CREATE TABLE `project` (
 INSERT INTO `project` VALUES (1,'egoing','학선_우리만남이.mp3','우리만남이','1절은 완성했는데 2절을 못하겠어요 좋은 아이디어 부탁드려요');
 INSERT INTO `project` VALUES (2,'taeho','music1.mp3','내가 살아있는 건','곡이 너무 무난해서 악기 추천해 주세요');
 INSERT INTO `project` VALUES (3,'duru','music2.mp3','falling','edm 쪽으로 멜로디 추천 부탁드려요');
+
+-- follow/unfollow 파일
+
+CREATE TABLE `Follow` (
+  `key` int(11) NOT NULL AUTO_INCREMENT,
+  `f_id` varchar(20) NOT NULL,
+  `audioPath` varchar(50) NOT NULL,
+  `title` varchar(20) NOT NULL,
+  `description` text,
+  PRIMARY KEY (`key`)
+);
+ 
+--
+-- Dumping data for table `project`
+--
+ 
+INSERT INTO `Follow` VALUES (1,'egoing','학선_우리만남이.mp3','우리만남이','1절은 완성했는데 2절을 못하겠어요 좋은 아이디어 부탁드려요');
+INSERT INTO `Follow` VALUES (2,'taeho','music1.mp3','내가 살아있는 건','곡이 너무 무난해서 악기 추천해 주세요');
+INSERT INTO `Follow` VALUES (3,'duru','music2.mp3','falling','edm 쪽으로 멜로디 추천 부탁드려요');
