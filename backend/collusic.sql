e9a3986207aa6c3c05fa58b74487e4cc9503a181
Daae-Kim <wlsemrdl1212@naver.com>
2021-01-27 13:01:01 +0900

    pull 전 collusic.sql 커밋

--- backend/collusic.sql
+++ backend/collusic.sql
@@ -76,19 +76,6 @@ CREATE TABLE `project` (
 -- Dumping data for table `project`
 --
  
-<<<<<<< HEAD
-INSERT INTO `project` VALUES (1,'egoing','학선_우리만남이.mp3','우리만남이','1절은 완성했는데 2절을 못하겠어요 좋은 아이디어 부탁드려요');
-INSERT INTO `project` VALUES (2,'taeho','music1.mp3','내가 살아있는 건','곡이 너무 무난해서 악기 추천해 주세요');
-INSERT INTO `project` VALUES (3,'duru','music2.mp3','falling','edm 쪽으로 멜로디 추천 부탁드려요');
-
--- follow/unfollow 파일
-
-CREATE TABLE `Follow` (
-  `key` int(11) NOT NULL AUTO_INCREMENT,
-  `f_id` varchar(20) NOT NULL,
-  `audioPath` varchar(50) NOT NULL,
-  `title` varchar(20) NOT NULL,
-=======
 INSERT INTO `project` VALUES (1,'egoing',1,'학선_우리만남이.mp3','우리만남이','1절은 완성했는데 2절을 못하겠어요 좋은 아이디어 부탁드려요');
 INSERT INTO `project` VALUES (2,'egoing',2,'musicismylife.mp3','우리만남이','1절은 완성했는데 2절을 못하겠어요 좋은 아이디어 부탁드려요');
 INSERT INTO `project` VALUES (3,'taeho',1,'music1.mp3','내가 살아있는 건','곡이 너무 무난해서 악기 추천해 주세요');
@@ -103,23 +90,13 @@ CREATE TABLE `commit` (
   `c_id` varchar(20) NOT NULL,
   `p_key` int(11) NOT NULL,
   `audioPath` varchar(50) NOT NULL,
->>>>>>> 1283e78e18c0f29fa7b0b8fd6cf0b46a985c0c33
   `description` text,
   PRIMARY KEY (`key`)
 );
  
 --
-<<<<<<< HEAD
--- Dumping data for table `project`
---
- 
-INSERT INTO `Follow` VALUES (1,'egoing','학선_우리만남이.mp3','우리만남이','1절은 완성했는데 2절을 못하겠어요 좋은 아이디어 부탁드려요');
-INSERT INTO `Follow` VALUES (2,'taeho','music1.mp3','내가 살아있는 건','곡이 너무 무난해서 악기 추천해 주세요');
-INSERT INTO `Follow` VALUES (3,'duru','music2.mp3','falling','edm 쪽으로 멜로디 추천 부탁드려요');
-=======
 -- Dumping data for table `commit`
 --
  
 INSERT INTO `commit` VALUES (1,'egoing','daae',1,'기여내용1.mp3','기타소리를 추가해봤어요 이건 어떠세요??');
 INSERT INTO `commit` VALUES (2,'taeho',`jeahyun`,2,'sindisizer.mp3','edm 느낌으로 추가해봤습니다');
->>>>>>> 1283e78e18c0f29fa7b0b8fd6cf0b46a985c0c33
