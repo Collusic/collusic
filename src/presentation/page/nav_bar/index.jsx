2416e3ede761397f054e53e84a8ea002a596e87f
spiderq2697 <spiderq10@gmail.com>
2021-07-20 12:45:12 +0900

    navbar 프로필 사진 클릭 시 /mypage로 path 설정

--- src/presentation/page/nav_bar/index.jsx
+++ src/presentation/page/nav_bar/index.jsx
@@ -2,14 +2,22 @@ import React from "react";
 import { StyledNavBar, Title, Box, Alarm, Profile } from "./styled";
 import imgAlarm from "assets/alarm_3x.png";
 import imgProfile from "assets/profile_3x.png";
+import useLastLocationHistory from "lib/history";
 
 function NavBar() {
+  const setHistory = useLastLocationHistory();
+
   return (
     <StyledNavBar>
       <Title>collusic</Title>
       <Box>
         <Alarm src={imgAlarm} />
-        <Profile src={imgProfile} />
+        <Profile
+          src={imgProfile}
+          onClick={() => {
+            setHistory("/mypage");
+          }}
+        />
       </Box>
     </StyledNavBar>
   );
