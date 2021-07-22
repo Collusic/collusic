9d5916ef843e4c1461da4934eeaacf2f1be76419
JUDONGHYEOK <65863017+JUDONGHYEOK@users.noreply.github.com>
2021-07-22 15:40:03 +0900

    feat 로그인/회원가입 폼 수정

--- src/presentation/page/home/LoginModal.js
+++ src/presentation/page/home/LoginModal.js
@@ -1,22 +1,62 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import { LoginModalContainer } from "./styled";
-import { Link } from "react-router-dom";
 
 const LoginModal = ({ closeModal, openSignInModal }) => {
+  const [loginForm, setLoginForm] = useState({
+    email: "",
+    password: "",
+  });
+  const onChange = useCallback(
+    (e) => {
+      setLoginForm({
+        ...loginForm,
+        [e.target.name]: e.target.value,
+      });
+    },
+    [loginForm]
+  );
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      var regExp =
+        /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+
+      if (!regExp.test(loginForm.email)) {
+      } else {
+      }
+    },
+    [loginForm]
+  );
   return (
     <LoginModalContainer>
-      <button onClick={closeModal} className="Modal__Button--Cancel">
-        x
-      </button>
-      <h2>Collusic</h2>
-      <input type="text" placeholder="이메일" />
-      <input type="password" placeholder="비밀번호" />
-      <Link to="/">
-        <button id="LoginModal__LoginButton">LOG IN</button>
-      </Link>
-      <button id="LoginModal__SignInButton" onClick={openSignInModal}>
-        회원가입
-      </button>
+      <form onSubmit={onSubmit} name="loginForm">
+        <button onClick={closeModal} className="Modal__Button--Cancel">
+          x
+        </button>
+        <h2>Collusic</h2>
+        <input
+          type="text"
+          id="LoginModal__email"
+          placeholder="이메일"
+          name="email"
+          value={loginForm.email}
+          onChange={onChange}
+        />
+        <input
+          type="password"
+          id="LoginModal__password"
+          placeholder="비밀번호"
+          name="password"
+          value={loginForm.password}
+          onChange={onChange}
+        />
+        <button id="LoginModal__LoginButton" type="submit">
+          LOG IN
+        </button>
+        <button id="LoginModal__SignInButton" onClick={openSignInModal}>
+          회원가입
+        </button>
+      </form>
     </LoginModalContainer>
   );
 };
