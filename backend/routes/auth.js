7434b04b6f6e546387bccc3204368c66a038890b
Daae-Kim <wlsemrdl1212@naver.com>
2021-07-27 16:21:53 +0900

    mypage json시도

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,38 +1,47 @@
-const express = require('express');
-const passport = require('passport');
-const bcrypt = require('bcrypt');
-const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
-const User = require('../models/user');
+const express = require("express");
+const passport = require("passport");
+const bcrypt = require("bcrypt");
+const { isLoggedIn, isNotLoggedIn } = require("./middlewares");
+const User = require("../models/user");
 
 const router = express.Router();
 
-router.post('/join', isNotLoggedIn, async (req, res, next) => {
+router.post("/join", isNotLoggedIn, async (req, res, next) => {
   const { email, nick, password } = req.body;
   try {
-    const exUser = await User.findOne({ where: { email } });//기존이메일로 가입한 사람이 있나?
+    const exUser = await User.findOne({ where: { email } }); //기존이메일로 가입한 사람이 있나?
     if (exUser) {
-      return res.redirect('/join?error=exist');//프론트에서 이미 가입한 메일이라고 알림
+      return res.status(401).json({
+        msg: "User already exist",
+        success: false,
+      }); //프론트에서 이미 가입한 메일이라고 알림
     }
-    const hash = await bcrypt.hash(password, 12);//기존이메일이 아니면 password를 해쉬화 해서 저장
+    const hash = await bcrypt.hash(password, 12); //기존이메일이 아니면 password를 해쉬화 해서 저장
     await User.create({
       email,
       nick,
       password: hash,
     });
-    return res.redirect('/');
+    return res.status(200).json({
+      msg: "SignIn Success",
+      success: true,
+      email: req.body.email,
+      nick: req.body.nick,
+    });
   } catch (error) {
     console.error(error);
     return next(error);
   }
 });
 
-router.post('/login', isNotLoggedIn, (req, res, next) => {
-  passport.authenticate('local', (authError, user, info) => {
+router.post("/login", isNotLoggedIn, (req, res, next) => {
+  passport.authenticate("local", (authError, user, info) => {
     if (authError) {
       console.error(authError);
       return next(authError);
     }
-    if (!user) {//로그인 실패한 경우
+    if (!user) {
+      //로그인 실패한 경우
       return res.redirect(`/?loginError=${info.message}`);
     }
     return req.login(user, (loginError) => {
@@ -40,23 +49,19 @@ router.post('/login', isNotLoggedIn, (req, res, next) => {
         console.error(loginError);
         return next(loginError);
       }
-      return res.redirect('/');
+
+      return res.status(200).json({
+        user: user,
+        email: req.user,
+      });
     });
   })(req, res, next); // 미들웨어 내의 미들웨어에는 (req, res, next) 꼭 붙여야함.
 });
 
-router.get('/logout', isLoggedIn, (req, res) => {
+router.get("/logout", isLoggedIn, (req, res) => {
   req.logout();
   req.session.destroy();
-  res.redirect('/');
-});
-
-router.get('/kakao', passport.authenticate('kakao'));
-
-router.get('/kakao/callback', passport.authenticate('kakao', {
-  failureRedirect: '/',
-}), (req, res) => {
-  res.redirect('/');
+  res.redirect("/");
 });
 
 module.exports = router;
