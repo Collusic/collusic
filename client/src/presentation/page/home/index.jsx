baa2a55710f63fe429aa4444b8b18c0dfde2e83e
JUDONGHYEOK <65863017+JUDONGHYEOK@users.noreply.github.com>
2021-07-28 21:17:50 +0900

    19브랜치 merge

--- client/src/presentation/page/home/index.jsx
+++ client/src/presentation/page/home/index.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Modal from "react-modal";
-import Login from "./loginModal";
+import Login from "./LoginModal";
 import Error from "./loginErrorModal";
 import BG from "../../../assets/bg.png";
-import SignIn from "./signInModal";
+import SignIn from "./SignInModal";
 import {
   StyledContainer,
   HomeImageContainer,
