1a524b5bcdc1f915dec7eb0d911fc5f91876314d
spiderq2697 <spiderq10@gmail.com>
2021-07-28 20:15:43 +0900

    create 라우터 재설정

--- src/presentation/App.js
+++ src/presentation/App.js
@@ -3,10 +3,10 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Main from "./page/main";
 import Home from "./page/home";
-import CreateProject from "./page/create";
 import NavBar from "./page/nav_bar";
 import DetailProject from "./page/detailProject";
 import CreateContributeProject from "./page/createContributeProject";
+import Create from "./page/create";
 
 function App() {
   return (
@@ -26,6 +26,9 @@ function App() {
         </Route>
         <Route exact path="/project/ContributeProject">
           <CreateContributeProject></CreateContributeProject>
+        </Route>
+        <Route exact path="/create">
+          <Create></Create>
         </Route>
       </Switch>
     </Router>
