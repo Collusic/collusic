337179a4dcb8d8297bd24aab1f61d9fa860ab58c
Daae-Kim <wlsemrdl1212@naver.com>
2021-07-29 23:28:33 +0900

    메인, 요청작생성, 기여작 생성, 요청작 상세페이지(진행중)

--- backend/routes/page.js
+++ backend/routes/page.js
@@ -9,6 +9,7 @@ const {
 const router = express.Router();
 const mypageController = require("../controllers/mypage");
 const requestProjectController = require("../controllers/requestProjects");
+const contributeProjectController = require("../controllers/contributeProjects");
 const { request } = require("express");
 router.get("/mypage", isLoggedIn, mypageController.readMyPageAPI);
 router.post(
@@ -25,6 +26,11 @@ router.get(
   "/requestProjects/:id/comments",
   isLoggedIn,
   requestProjectController.commentsAPI
+);
+router.post(
+  "/requestProjects/:id/comments",
+  upload_commit.single("data"),
+  contributeProjectController.createContriProjectAPI
 );
 module.exports = router;
 // router.get("/", async (req, res, next) => {
